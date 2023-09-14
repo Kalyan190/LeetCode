cc0c33e7cf6f461ca1755d0032f5f3feeb154496
KALYAN KUMAR <118277849+Kalyan190@users.noreply.github.com>
2023-09-14 20:30:24 +0530

    Time: 2 ms (47.24%), Space: 6.3 MB (12.75%) - LeetHub

--- 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.cpp
+++ 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.cpp
@@ -3,23 +3,28 @@ public:
     bool isSameAfterReversals(int num) {
         if(num == 0)
         return true;
+
+        if(num%10 == 0)
+        return false;
+
+        return true;
         
-        int rev1=0,rev2,n;
-        n = num;
-        while(n>0){
-           rev1 = rev1*10 + n%10 ;
-           n /=10;
+    //     int rev1=0,rev2,n;
+    //     n = num;
+    //     while(n>0){
+    //        rev1 = rev1*10 + n%10 ;
+    //        n /=10;
            
-        }
-        rev2 = rev1;
-        int ans = 0;
-        while(rev2>0){
-        ans = ans*10 + rev2%10 ;
-        rev2 /=10;                                 
+    //     }
+    //     rev2 = rev1;
+    //     int ans = 0;
+    //     while(rev2>0){
+    //     ans = ans*10 + rev2%10 ;
+    //     rev2 /=10;                                 
+    //  }
+    //      if(ans == num)
+    //      return true;
+    //      else
+    //      return false;
      }
-         if(ans == num)
-         return true;
-         else
-         return false;
-    }
 };
