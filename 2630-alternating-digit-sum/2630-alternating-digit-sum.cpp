46638d20ea0dbbe7fc625f7f9d23c7535fbf2bed
KALYAN KUMAR <118277849+Kalyan190@users.noreply.github.com>
2023-09-17 14:53:42 +0530

    Time: 2 ms (47.17%), Space: 6.2 MB (12.36%) - LeetHub

---
+++ 2630-alternating-digit-sum/2630-alternating-digit-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int alternateDigitSum(int n) {
+      int sumEven = 0, sumOdd = 0, count = 0;
+        
+        while(n>0){
+            if(count%2)
+            sumOdd += n%10;
+            else
+            sumEven += n%10;
+            count++;
+            n /= 10; 
+        }
+    
+        return (count%2)?sumEven - sumOdd:sumOdd - sumEven;
+    }
+};
