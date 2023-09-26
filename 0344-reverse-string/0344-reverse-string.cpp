c4a44e46ffdd30a9fe28faa69c3fad12c756be2d
KALYAN KUMAR <118277849+Kalyan190@users.noreply.github.com>
2023-09-26 19:45:28 +0530

    Time: 19 ms (51.16%), Space: 23.6 MB (12.74%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -8,11 +8,11 @@ public:
         // }
         //second method;
         int start = 0, end = s.size()-1;
-        while(start<end){
+        while(start<end)//{
              swap(s[start++],s[end--]);
             //  start++;
             //  end--;
-        }
+       // }
 
         
 
