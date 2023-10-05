992a214325c1ef0f7335d00b495a426ab48982e3
KALYAN KUMAR <118277849+Kalyan190@users.noreply.github.com>
2023-10-05 21:56:54 +0530

    Time: 9 ms (55.09%), Space: 22.2 MB (5.71%) - LeetHub

--- 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.cpp
+++ 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.cpp
@@ -5,11 +5,9 @@ public:
         for(int i = 0; i<nums.size(); i++){
             int count = 0, temp = i;
             while(temp){
-                // count += temp & 1;
-                // temp >>= 1;
-                if(temp%2 == 1)
-                count++;
-                temp /= 2;
+                count += temp & 1;
+                temp >>= 1;
+               
             }
             if(count == k){
                 ans += nums[i];
