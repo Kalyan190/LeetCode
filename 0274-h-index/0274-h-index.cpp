1a4b00589641074e9bd1f955428218d0a9b5d556
KALYAN KUMAR <118277849+Kalyan190@users.noreply.github.com>
2024-06-07 14:21:57 +0530

    Time: 6 ms (19.56%), Space: 10.7 MB (54.59%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -2,14 +2,32 @@ class Solution {
 public:
     int hIndex(vector<int>& citi) {
         sort(citi.begin(),citi.end());
+        // Brute force solve--------------
+        // int count = 0;
+        // int n = citi.size();
+        // for(int i = 0; i<n; i++){
+        //     if(citi[i] >= n-i){
+        //         count = max(count,n-i);
+        //     }
+        // }
+        // return count;
 
-        int count = 0;
+        // Binary search --------------
+
         int n = citi.size();
-        for(int i = 0; i<n; i++){
-            if(citi[i] >= n-i){
-                count = max(count,n-i);
+        int s = 0,e = n-1;
+        int ans = 0;
+
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            if(citi[mid] >= n-mid){
+                ans = max(ans,n-mid);
+                e = mid-1;
+            }
+            else{
+                s = mid+1;
             }
         }
-        return count;
+        return ans;
     }
 };
