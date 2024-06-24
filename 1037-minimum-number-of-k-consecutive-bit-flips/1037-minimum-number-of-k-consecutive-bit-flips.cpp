bfacbb66840a7727c7cf9b76c2c56883db201f50
KALYAN KUMAR <118277849+Kalyan190@users.noreply.github.com>
2024-06-24 21:27:06 +0530

    Time: 98 ms (32.58%), Space: 111.9 MB (44.47%) - LeetHub

--- 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
-        int curr = 0, res = 0, n = nums.size();
+        // int curr = 0, res = 0, n = nums.size();
+        // for(int i = 0; i<n; i++){
+        //     if(i>= k && nums[i-k] > 1){
+        //         curr--;
+        //         nums[i-k] -= 2;
+        //     }
+        //     if(curr%2 == nums[i]){
+        //         if(i+k > n) return -1;
+        //         nums[i] += 2;
+        //         curr++;
+        //         res++;
+        //     }
+        // }
+        // return res;
+
+        // Another method to solve
+
+        int ans = 0, n = nums.size();
+        queue<int>record;
+
         for(int i = 0; i<n; i++){
-            if(i>= k && nums[i-k] > 1){
-                curr--;
-                nums[i-k] -= 2;
+            int pivot = record.size() % 2 == 0 ? nums[i] : 1 - nums[i];
+            if(pivot == 0){
+                ans++;
+                record.push(i + k - 1);
             }
-            if(curr%2 == nums[i]){
-                if(i+k > n) return -1;
-                nums[i] += 2;
-                curr++;
-                res++;
-            }
+            if(!record.empty() && i == record.front())
+            record.pop();
         }
-        return res;
+        return record.empty() ? ans : -1;
     }
 };
