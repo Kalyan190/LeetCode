6078dced722d7cfa98c53a0ce1fc043bab57e74e
KALYAN KUMAR <118277849+Kalyan190@users.noreply.github.com>
2024-07-29 12:43:16 +0530

    Time: 33 ms (62.38%), Space: 11.6 MB (51.74%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.cpp
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -4,16 +4,42 @@ public:
         int count = 0;
         int n = rating.size();
         
+        // Brute force solve
+        // for(int i = 0; i<n-2; i++){
+        //     for(int j = i+1; j<n-1;j++){
+        //         for(int k = j+1; k<n; k++){
+        //             if((rating[i]> rating[j] && rating[j] > rating[k]) || (rating[i]< rating[j] && rating[j] < rating[k])){
+        //                 count++;
+        //             }
+        //         }
+        //     }
+        // }
+        // return count;
 
-        for(int i = 0; i<n-2; i++){
-            for(int j = i+1; j<n-1;j++){
-                for(int k = j+1; k<n; k++){
-                    if((rating[i]> rating[j] && rating[j] > rating[k]) || (rating[i]< rating[j] && rating[j] < rating[k])){
-                        count++;
-                    }
+        // optimise method
+        for(int i = 1; i<n-1; i++){
+            int leftSmall = 0, leftLarge = 0;
+            int rightSmall = 0, rightLarge = 0;
+
+            for(int j = 0; j<i; j++){
+                if(rating[j] < rating[i]){
+                    leftSmall++;
+                }
+                if(rating[j] > rating[i]){
+                    leftLarge++;
                 }
             }
+            for(int j = i+1; j<n; j++){
+                if(rating[j]<rating[i]){
+                    rightSmall++;
+                }
+                if(rating[j] > rating[i]){
+                    rightLarge++;
+                }
+            }
+            count += leftSmall*rightLarge + leftLarge*rightSmall;
         }
         return count;
+
     }
 };
