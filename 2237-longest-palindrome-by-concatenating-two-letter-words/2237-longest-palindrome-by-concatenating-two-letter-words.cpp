fc036660ff70c452779a43c54dc35c8c1ab970f6
KALYAN KUMAR <118277849+Kalyan190@users.noreply.github.com>
2024-08-13 01:08:04 +0530

    Time: 196 ms (96.08%), Space: 172.2 MB (22.66%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,32 +1,49 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
-        unordered_map<string,int>mp;
+        // unordered_map<string,int>mp;
 
-        int unpaired = 0,ans = 0;
+        // int unpaired = 0,ans = 0;
+        // for(string w: words){
+        //     if(w[0] == w[1]){
+        //         if(mp[w]>0){
+        //             unpaired--;
+        //             mp[w]--;
+        //             ans += 4;
+        //         }else{
+        //             mp[w]++;
+        //             unpaired++;
+        //         }
+        //     }else{
+        //         string rev = w;
+        //         reverse(rev.begin(),rev.end());
+        //         if(mp[rev]>0){
+        //             ans += 4;
+        //             mp[rev]--;
+        //         }else{
+        //             mp[w]++;
+        //         }
+        //     }
+        // }
+            
+        // if(unpaired > 0) ans += 2;
+        // return ans;
+
+        // without map solve
+        vector<vector<int>>counter(26,vector<int>(26,0));
+
+        int ans = 0;
         for(string w: words){
-            if(w[0] == w[1]){
-                if(mp[w]>0){
-                    unpaired--;
-                    mp[w]--;
-                    ans += 4;
-                }else{
-                    mp[w]++;
-                    unpaired++;
-                }
-            }else{
-                string rev = w;
-                reverse(rev.begin(),rev.end());
-                if(mp[rev]>0){
-                    ans += 4;
-                    mp[rev]--;
-                }else{
-                    mp[w]++;
-                }
+            int a = w[0] - 'a', b = w[1] - 'a';
+            if(counter[b][a]) ans += 4, counter[b][a]--;
+            else counter[a][b]++;
+        }
+        for(int i = 0; i<26; i++){
+            if(counter[i][i]){
+                ans += 2;
+                break;
             }
         }
-            
-        if(unpaired > 0) ans += 2;
         return ans;
     }
 };
