ecfef7aa2e4b0c78e5afc3a26c6cf23e55651eff
KALYAN KUMAR <118277849+Kalyan190@users.noreply.github.com>
2024-08-16 00:03:34 +0530

    Time: 50 ms (98.67%), Space: 85.8 MB (63.29%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int fives = 0,tens = 0;
-        for(int i = 0; i<bills.size(); i++){
-            if(bills[i] == 5)
-               fives++;
-            else if(bills[i] == 10){
-                if(fives == 0){
-                    return false;
-                }
-                fives--;
-                tens++;
-            }
-            else{
-                if(tens > 0 && fives > 0){
-                    tens--;
-                    fives--;
-                }
-                else if(fives >= 3){
-                    fives -= 3;
+        // int fives = 0,tens = 0;
+        // for(int i = 0; i<bills.size(); i++){
+        //     if(bills[i] == 5)
+        //        fives++;
+        //     else if(bills[i] == 10){
+        //         if(fives == 0){
+        //             return false;
+        //         }
+        //         fives--;
+        //         tens++;
+        //     }
+        //     else{
+        //         if(tens > 0 && fives > 0){
+        //             tens--;
+        //             fives--;
+        //         }
+        //         else if(fives >= 3){
+        //             fives -= 3;
 
-                }
-                else{
-                    return false;
-                }
-            }
+        //         }
+        //         else{
+        //             return false;
+        //         }
+        //     }
             
+        // }
+        // return true;
+
+        int five = 0, ten = 0;
+
+        for(int i : bills){
+            if(i == 5) five++;
+            else if(i == 10) five--, ten++;
+            else if(ten > 0) ten--, five--;
+            else five -= 3;
+            if(five < 0) return false;
         }
         return true;
     }
