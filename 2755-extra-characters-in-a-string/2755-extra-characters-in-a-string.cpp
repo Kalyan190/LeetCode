4511831d402bcd81e41292c78646fc571494b842
KALYAN KUMAR <118277849+Kalyan190@users.noreply.github.com>
2024-09-23 17:26:31 +0530

    Time: 40 ms (99.16%), Space: 60.4 MB (98.99%) - LeetHub

---
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        memset(dp,-1,sizeof(dp));
+
+        return minExtraCharHelper(s,dictionary,0);
+    }
+private:
+   int dp[51];
+   int minExtraCharHelper(const string &s, vector<string>&dict,int i){
+     if(i == s.size())
+       return 0;
+
+      if(dp[i] == -1){
+        dp[i] = 1 + minExtraCharHelper(s,dict,i + 1);
+
+        for(const auto &w: dict){
+            if(s.compare(i,w.size(),w) == 0){
+                dp[i] = min(dp[i],minExtraCharHelper(s,dict,i + w.size()));
+            }
+        }
+       
+      }
+       return dp[i];
+   }
+};
