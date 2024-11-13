9bd7ee01a9a69f2db9920971a0bbaaf88863165c
KALYAN KUMAR <118277849+Kalyan190@users.noreply.github.com>
2024-11-13 10:15:37 +0530

    Time: 71 ms (32.14%), Space: 60.3 MB (61.66%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -9,6 +9,7 @@ public:
             int low = lower_bound(begin(nums) + i+1,end(nums),lower - nums[i]) - begin(nums);
             int high = upper_bound(begin(nums) + i + 1,end(nums), upper - nums[i]) - begin(nums);
             ans += 1LL*(high - low);
+            // cout<<"index-> "<< low <<" "<< high<<endl;
         }
         return ans;
     }
